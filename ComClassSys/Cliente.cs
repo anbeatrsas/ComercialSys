b30dff81471fce4f93c3345e630c552378d4f916
Ennie <anabeatrizalmeida004@gmail.com>
2024-05-24 11:56:02 -0300

    Criando clientes e endereco

--- ComClassSys/Cliente.cs
+++ ComClassSys/Cliente.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,66 @@ namespace ComClassSys
 {
     public class Cliente
     {
-    }
+
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Cpf { get; set; }
+        public string? Telefone { get; set; }
+        public string? Email { get; set; }
+        public string? Data_nasc { get; set; }
+        public bool Ativo { get; set; }
+        public List<Endereco> Enderecos { get; set; }
+
+
+
+
+        public Cliente()
+        {
+
+        }
+
+        public Cliente(string nome, string cpf, string telefone, string email, string data_nasc) 
+        {
+
+            Nome = nome;
+            Cpf = cpf;
+            Telefone = telefone;
+            Email = email;
+            Data_nasc = data_nasc;
+        
+        }
+
+        public Cliente(int id, string nome, string cpf, string telefone, string email, string data_nasc, bool ativo)
+        {
+
+            Id = id;
+            Nome = nome;
+            Cpf = cpf;
+            Telefone = telefone;
+            Email = email;
+            Data_nasc = data_nasc;
+            Ativo = ativo;
+
+        }
+
+        public void Inserir()
+        {
+
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "sp_insere_cliente";
+
+            cmd.Parameters.AddWithValue("sp_nome", Nome);
+            cmd.Parameters.AddWithValue("sp_cpf", Cpf);
+            cmd.Parameters.AddWithValue("sp_telefone", Telefone);
+            cmd.Parameters.AddWithValue("sp_email", Email);
+            cmd.Parameters.AddWithValue("sp_data_nasc", Data_nasc);
+
+            cmd.ExecuteNonQuery();
+
+        }
+
+
+
+    }       
 }
--- ComClassSys/Endereco.cs
+++ ComClassSys/Endereco.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +11,65 @@ namespace ComClassSys
 {
     public class Endereco
     {
+
+        public int Id { get; set; }
+        public string Cep { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; } 
+        public string? Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Uf { get; set; }
+        public string  Tipo_endereco { get; set; }
+
+        public Endereco() { }
+
+        public Endereco(string cep, string logradouro, string numero, string complemento, string bairro, string cidade, string uf)
+        {
+
+            Cep = cep;
+            Logradouro = logradouro;
+            Numero = numero;
+            Complemento = complemento;
+            Bairro = bairro;
+            Cidade = cidade;
+            Uf = uf;
+        
+        }
+
+        public Endereco(int id, string cep, string logradouro, string numero, string complemento, string bairro, string cidade, string uf, string tipo_endereco)
+        {
+
+            Id = id;
+            Cep = cep;
+            Logradouro = logradouro;
+            Numero = numero;
+            Complemento = complemento;
+            Bairro = bairro;
+            Cidade = cidade;
+            Uf = uf;
+            Tipo_endereco = tipo_endereco;
+
+        }
+
+        public void Inserir()
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "sp_endereco_insert";
+
+            cmd.Parameters.AddWithValue("spcep", Cep);
+            cmd.Parameters.AddWithValue("splogradouro", Logradouro);
+            cmd.Parameters.AddWithValue("spnumero", Numero);
+            cmd.Parameters.AddWithValue("spcomplemento", Complemento);
+            cmd.Parameters.AddWithValue("spbairro", Bairro);
+            cmd.Parameters.AddWithValue("spcidade", Cidade);
+            cmd.Parameters.AddWithValue("spuf", Uf);
+
+
+            cmd.ExecuteNonQuery();
+
+        }
+
     }
 }
