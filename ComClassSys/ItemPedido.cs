a1d30ab968824762ca5576ddc124fb6b8d8a9713
ANBEATRSAS <anabeatrizalmeida004@gmail.com>
2024-06-20 11:45:05 -0300

    PEDIDO ITEM PEDIDO

--- ComClassSys/ItemPedido.cs
+++ ComClassSys/ItemPedido.cs
@@ -1,12 +1,113 @@
 ﻿using System;
+using System.Data;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace ComClassSys
 {
     public class ItemPedido
     {
+        
+
+        public int Id { get; set; }
+        public Pedido? Pedido { get; set; }
+        public Produto Produto { get; set; }
+        public double ValorUnit { get; set; }
+        public double Quantidade { get; set; }
+        public double Desconto { get; set; }
+
+        public ItemPedido(int id, Pedido pedido, Produto produto, double valorUnit, double quantidade, double desconto)
+        {
+            Id = id;
+            Pedido = pedido;
+            Produto = produto;
+            ValorUnit = valorUnit;
+            Quantidade = quantidade;
+            Desconto = desconto;
+        }
+
+        public ItemPedido()
+        {
+
+            Pedido = new();
+
+        }
+
+        public ItemPedido(Pedido pedido, Produto produto, double valorUnit, double quantidade, double desconto)
+        {
+            Pedido = pedido;
+            Produto = produto;
+            ValorUnit = valorUnit;
+            Quantidade = quantidade;
+            Desconto = desconto;
+        }
+
+
+        // métodos Manter
+        // Inserir Item 
+        public void Inserir()
+        {
+
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "sp_itempedido_insert";
+            cmd.Parameters.AddWithValue("sppedido_id", Pedido.Id);
+            cmd.Parameters.AddWithValue("spproduto_id", Produto.Id);
+            cmd.Parameters.AddWithValue("spquantidade", Quantidade);
+            cmd.Parameters.AddWithValue("spdesconto", Desconto);
+
+
+            Id = Convert.ToInt32(cmd.ExecuteScalar);
+
+        }
+
+
+        // Consultar Item de pedido (Obter por id ped e id prod)
+        public static ItemPedido ObterItem(int idPedido, int idProduto=0)
+        {
+
+            ItemPedido itemPedido = new();
+
+            return itemPedido;
+
+        }
+
+
+        public static List<ItemPedido> ObterListaPorPedido(int idPedido)
+        {
+
+            List<ItemPedido> itens = new();
+
+            return itens;
+
+        }
+
+
+        // alterar item
+        public bool Alterar(int id)
+        {
+
+
+            return true;
+        }
+
+
+        // Excluir
+        public void Remover(int id)
+        {
+
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "sp_itempedido_delete";
+
+            cmd.Parameters.AddWithValue("spid", Id);
+            cmd.ExecuteNonQuery();
+
+        }
+
+
     }
 }
--- ComClassSys/Pedido.cs
+++ ComClassSys/Pedido.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mysqlx.Prepare;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,133 @@ namespace ComClassSys
 {
     public class Pedido
     {
+        
+
+        public int Id { get; set; }
+        public Usuario Usuario { get; set; }
+        public Cliente Cliente { get; set; }
+        public DateTime Data { get; set; }
+        public string Status { get; set; }
+        public double Desconto { get; set; }
+        List<ItemPedido> Itens { get; set; }
+
+
+        public Pedido(int id, Usuario usuario, Cliente cliente, DateTime data, string status, double desconto)
+        {
+            Id = id;
+            Usuario = usuario;
+            Cliente = cliente;
+            Data = data;
+            Status = status;
+            Desconto = desconto;
+            
+        }
+
+        public Pedido()
+        {
+
+        }
+
+        public Pedido(int id, Usuario usuario, Cliente cliente, DateTime data, string status, double desconto, List<ItemPedido> itens)
+        {
+            Id = id;
+            Usuario = usuario;
+            Cliente = cliente;
+            Data = data;
+            Status = status;
+            Desconto = desconto;
+            Itens = Itens;
+
+        }
+
+        public Pedido(Usuario usuario, Cliente cliente, DateTime data, string status, double desconto)
+        {
+            Usuario = usuario;
+            Cliente = cliente;
+            Data = data;
+            Status = status;
+            Desconto = desconto;
+        }
+
+
+        public void Inserir()
+        {
+
+            var cmd = Banco.Abrir();
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.CommandText = "sp_pedido_insert";
+            cmd.Parameters.AddWithValue("spusuario_id", Usuario.Id);
+            cmd.Parameters.AddWithValue("spcliente_id", Cliente.Id);
+            cmd.Parameters.AddWithValue("spstatus", Status);
+            cmd.Parameters.AddWithValue("spdesconto", Desconto);
+
+            Id = Convert.ToInt32(cmd.ExecuteScalar());
+
+        }
+
+        public static Pedido ObterPorId(int id)
+        {
+
+            Pedido pedido = new();
+            var cmd = Banco.Abrir();
+            cmd.CommandText = $"SELECT * FROM pedidos WHERE id = {id}";
+            var dr = cmd.ExecuteReader(); // colecao de resultados que obtem da consulta
+
+            while (dr.Read())
+            {
+
+                pedido = new(dr.GetInt32(0),
+                    Usuario.ObterPorId(dr.GetInt32(1)),
+                    Cliente.ObterPorId(dr.GetInt32(2)),
+                    dr.GetDateTime(3),
+                    dr.GetString(4),
+                    dr.GetDouble(5),
+                    ItemPedido.ObterListaPorPedido(dr.GetInt32(0))
+                    ); 
+
+            }
+
+            return pedido;
+
+        }
+
+        public static Pedido ObterPorClienteId(int id)
+        {
+
+            Pedido pedido = new();
+
+            return pedido;
+
+
+        }
+
+        public List<Pedido> ObterLista()
+        {
+            List<Pedido> lista = new();
+
+            return lista;
+
+        }
+
+        public bool Alterar()
+        {
+            return true;
+        }
+
+        public bool Alterar(string status)
+        {
+            return true;
+        }
+
+        public static double CalcularPedido(int id)
+        {
+
+            return 0.0;
+
+        }
+
+
     }
+
+
 }
